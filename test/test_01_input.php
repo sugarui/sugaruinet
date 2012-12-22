0c35d775d1b719b6d7f62c319f15124d84099feb
sugarui-macos <sugarui@naver.com>
2012-12-22 17:33:12 +0900

    도서관/22토/나머지파일 커밋안하니까 깃헙싱크를 못함. 그래서 커밋함.

--- test/test_01_input.php
+++ test/test_01_input.php
@@ -2,25 +2,16 @@
 	
 	<?php
 	include_once ('../../db.php');
-
-			$bbb = mysqli_affected_rows($link);
-			$bbb = mysql_query($sql);
-			echo '<br><br><br>';
-			echo 'thenumberofrow affected<br>';
-			echo $bbb;
-			echo '<br><br><br>';
 			
 	if (!empty($_POST['aaa'])){
-		// $sql = 'INSERT INTO `codingeverybody` (`name`) VALUES (\''.mysql_real_escape_string($_POST['name']).'\')'; //베낀
 		$sql = 'INSERT INTO `table_test_01` (`text`) VALUES (\''.mysql_real_escape_string($_POST['aaa']).'\')'; //쿼리문
-		// $result = mysql_query($sql); // 베낀
 		$result = mysql_query($sql); //쿼리문 보냄
 		//쿼리가 잘 안갈때 오류출력
-		// if (!$result) {
-			// $message = "Error: " . mysql_error() . "<br>";
-			// $message = "Errored Query: ".$sql;
-			// die ($message);
-		// }
+		if (!$result) {
+			$message = "Error: " . mysql_error() . "<br>";
+			$message = "Errored Query: ".$sql;
+			die ($message);
+		}
 	}	
 	?>
 
